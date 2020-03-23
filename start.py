9674b401bb5b7d441d1255e9334e97ef0e36a266
simon <simonmdsn@gmail.com>
2020-03-23 11:20:47 +0100

    init

---
+++ start.py
@@ -0,0 +1,21 @@
+from flask import Flask, escape, render_template
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/corona')
+def corona():
+    URL = "https://www.sst.dk/da/corona/tal-og-overvaagning"
+    webpage = requests.get(URL)
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    html_tables = soup.findAll(class_ = "table-responsive")
+    paragraphs = ""
+    for table in html_tables:
+        paragraphs = paragraphs + str(table)
+    return paragraphs
+git init
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost")
